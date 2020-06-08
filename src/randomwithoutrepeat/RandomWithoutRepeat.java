b3021b19f30273018d271073c1892a36ed92f1bd
LEONARDO DE LIMA JUNIOR <juhhvoid@gmail.com>
2020-06-08 10:18:15 -0300

    commit 3

--- src/randomwithoutrepeat/RandomWithoutRepeat.java
+++ src/randomwithoutrepeat/RandomWithoutRepeat.java
@@ -7,22 +7,21 @@ import java.util.Random;
 public class RandomWithoutRepeat {
 
     public static void main(String[] args) {
-        int numero = 0;
         int find = 0;
         int c, i = 0; 
         ArrayList num = new ArrayList();
         Random rdm = new Random();
 
 //construindo o array sem repetição
-        for (i = 0; i < 15; i++) {
-            find = rdm.nextInt(25) + 1;
+        for (i = 0; i < 5; i++) {
+            find = rdm.nextInt(80) + 1;
             if (i == 0) {
                 num.add(find);
             } else {
                 c = 0;
                 while (c < i) {
                     if (num.get(c).equals(find)) {
-                        find = rdm.nextInt(15) + 1;
+                        find = rdm.nextInt(5) + 1;
                         c = 0;
                     } else {
                         c++;
