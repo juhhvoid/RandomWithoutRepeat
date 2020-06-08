a33d2ae19cee00ba1e910c7025d1fb525898381c
LEONARDO DE LIMA JUNIOR <juhhvoid@gmail.com>
2020-06-08 09:54:02 -0300

    commit 2

--- src/randomwithoutrepeat/RandomWithoutRepeat.java
+++ src/randomwithoutrepeat/RandomWithoutRepeat.java
@@ -9,12 +9,11 @@ public class RandomWithoutRepeat {
     public static void main(String[] args) {
         int numero = 0;
         int find = 0;
-        int c, i = 0;
-//int[] num = new int[15]; 
+        int c, i = 0; 
         ArrayList num = new ArrayList();
         Random rdm = new Random();
 
-//contruindo o array sem repetição
+//construindo o array sem repetição
         for (i = 0; i < 15; i++) {
             find = rdm.nextInt(25) + 1;
             if (i == 0) {
@@ -31,9 +30,10 @@ public class RandomWithoutRepeat {
                 }
                 num.add(find);
             }
+            //Ordenando a lista
             Collections.sort(num);
         }
-
+        //imprimindo a lista
         System.out.print(num);
 
     }
